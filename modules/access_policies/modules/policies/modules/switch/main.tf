ba692092838b0df45d4277213ccf6ab8af161d53
Richard <richwats@cisco.com>
2022-07-15 12:02:53 +1000

    name change

--- modules/access_policies/modules/policies/modules/switch/main.tf
+++ modules/access_policies/modules/policies/modules/switch/main.tf
@@ -33,37 +33,37 @@ NO support for:
 locals {
   ### Policy Name => ID Map ###
   policy_map = merge({
-    for k,p in var.switch.vpc_domain :
+    for k,p in var.switch.vpc_domains :
       k => {
         name = p.name
         type = "vpc_domain"
-        id = module.vpc_domain[k].policy_id
+        id = module.vpc_domains[k].policy_id
       }
   },
   {
-    for k,p in var.switch.vpc_protection_group :
+    for k,p in var.switch.vpc_protection_groups :
       k => {
         name = p.name
         type = "vpc_protection_group"
-        id = module.vpc_protection_group[k].policy_id
+        id = module.vpc_protection_groups[k].policy_id
       }
   }
   )
 }
 
 ### ACI Fabric Access Policy - Switch - VPC Domain Policy Module ###
-module "vpc_domain" {
+module "vpc_domains" {
   for_each = var.switch.vpc_domains
-  source = "./modules/vpc_domain"
+  source = "./modules/vpc_domains"
 
   ### VARIABLES ###
   vpc_domain = each.value
 }
 
 ### ACI Fabric Access Policy - Switch - VPC Explicit Protection Group Policy Module ###
-module "vpc_protection_group" {
+module "vpc_protection_groups" {
   for_each = var.switch.vpc_protection_groups
-  source = "./modules/vpc_protection_group"
+  source = "./modules/vpc_protection_groups"
 
   ### VARIABLES ###
   vpc_protection_group = each.value
