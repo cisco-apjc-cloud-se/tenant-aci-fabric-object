e42c6e5b2edc107e741e554aaa0bcf1dc5839c9b
Richard <richwats@cisco.com>
2022-07-15 16:35:40 +1000

    switch numbers

--- modules/access_policies/variables.tf
+++ modules/access_policies/variables.tf
@@ -295,8 +295,8 @@ variable "access_policies" {
         }))
         vpc_protection_groups = map(object({
           name                              = string # (Required) Name of Object VPC Explicit Protection Group.
-          switch1                           = string # (Required) Node Id of switch 1 to attach.
-          switch2                           = string # (Required) Node Id of switch 2 to attach.
+          switch1                           = number # (Required) Node Id of switch 1 to attach.
+          switch2                           = number # (Required) Node Id of switch 2 to attach.
           annotation                        = optional(string) # (Optional) Annotation for object VPC Explicit Protection Group.
           vpc_domain_policy                 = optional(string) # (Optional) VPC domain policy name.
           vpc_explicit_protection_group_id  = optional(number) # (Optional) Explicit protection group ID. Integer values are allowed between 1-1000. default value is "0".
